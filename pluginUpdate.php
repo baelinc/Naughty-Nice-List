0be3bdafed679b5996c23f637e7328572bb24155
baelinc <11509732+baelinc@users.noreply.github.com>
2024-09-09 09:51:45 -0500

    Update pluginUpdate.php

--- pluginUpdate.php
+++ pluginUpdate.php
@@ -1,6 +1,16 @@
-<div id="pluginupdate" class="text-center" style="display: none;">
-    <div class="updateTable text-success fppPluginEntryUpdateStatus">
-        <i class="fas fa-exclamation-circle"></i> <b>Plugin Update Available</b>
-    </div>
-    <a href="plugins.php" class="buttons btn-success"><i class="far fa-arrow-alt-circle-down"></i> Update Now</a>
-</div>
+<?php
+// Update script for the Naughty-Nice List plugin
+$pluginUpdateUrl = 'https://your-update-server.com/update';
+$localVersion = '1.0.0'; // Update this as per plugin version
+
+$updateAvailable = checkForPluginUpdate($pluginUpdateUrl, $localVersion);
+if ($updateAvailable) {
+    echo '<p>An update is available. <a href="' . $pluginUpdateUrl . '">Download here</a></p>';
+}
+
+function checkForPluginUpdate($url, $currentVersion) {
+    // Logic to check for updates
+    $latestVersion = '1.1.0'; // Simulate fetching from update server
+    return version_compare($latestVersion, $currentVersion, '>');
+}
+?>
