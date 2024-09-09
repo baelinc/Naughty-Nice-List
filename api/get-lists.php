d97590de69623ce767d73ca1ee5aeca529958984
baelinc <11509732+baelinc@users.noreply.github.com>
2024-09-09 09:59:36 -0500

    Update get-lists.php

--- api/get-lists.php
+++ api/get-lists.php
@@ -1,6 +1,9 @@
 <?php
-// Ensure no PHP errors are displayed as HTML
-ini_set('display_errors', 0); // Turn off error reporting
+// Turn off error display but log errors
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', '/path/to/your/error_log.log'); // Set this to your actual log file path
+
 header('Content-Type: application/json');
 
 // Fetch the naughty and nice lists from the settings
@@ -8,13 +11,14 @@ $data = convertAndGetSettings('naughty-nice-list');
 
 // Check if data is available and properly formatted
 if ($data && isset($data['naughty']) && isset($data['nice'])) {
+    // Prepare the response data
     $response = [
         'naughty' => array_slice($data['naughty'], 0, 5), // Limit to last 5
         'nice' => array_slice($data['nice'], 0, 5) // Limit to last 5
     ];
     echo json_encode($response);
 } else {
-    // Return an empty JSON response in case of an issue
+    // Return an error JSON response
     echo json_encode(['error' => 'Unable to retrieve lists']);
 }
 ?>
