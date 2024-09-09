ed5772c4816f221bd3c59eda17b36a2493bc1319
baelinc <11509732+baelinc@users.noreply.github.com>
2024-09-09 09:29:21 -0500

    Create pluginUpdate.php

---
+++ pluginUpdate.php
@@ -0,0 +1,6 @@
+<div id="pluginupdate" class="text-center" style="display: none;">
+    <div class="updateTable text-success fppPluginEntryUpdateStatus">
+        <i class="fas fa-exclamation-circle"></i> <b>Plugin Update Available</b>
+    </div>
+    <a href="plugins.php" class="buttons btn-success"><i class="far fa-arrow-alt-circle-down"></i> Update Now</a>
+</div>
