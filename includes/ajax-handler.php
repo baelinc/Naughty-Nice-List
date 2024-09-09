9da0f008399dc9443122f7c0f38b98b94eb5cae4
baelinc <11509732+baelinc@users.noreply.github.com>
2024-09-09 10:40:23 -0500

    Update ajax-handler.php

--- includes/ajax-handler.php
+++ includes/ajax-handler.php
@@ -3,18 +3,23 @@
 ini_set('display_errors', 0); // Turn off error reporting
 header('Content-Type: application/json');
 
-// Fetch the naughty and nice lists from the settings
-$data = convertAndGetSettings('naughty-nice-list');
+// Path to the JSON file
+$jsonFilePath = '/path/to/your/naughty-nice-list.json';
 
-// Check if data is available and properly formatted
-if ($data && isset($data['naughty']) && isset($data['nice'])) {
-    $response = [
-        'naughty' => array_slice($data['naughty'], 0, 5), // Limit to last 5
-        'nice' => array_slice($data['nice'], 0, 5) // Limit to last 5
-    ];
-    echo json_encode($response);
+// Check if the file exists
+if (file_exists($jsonFilePath)) {
+    $data = json_decode(file_get_contents($jsonFilePath), true);
+
+    if ($data && isset($data['naughty']) && isset($data['nice'])) {
+        $response = [
+            'naughty' => array_slice($data['naughty'], 0, 5), // Limit to last 5
+            'nice' => array_slice($data['nice'], 0, 5) // Limit to last 5
+        ];
+        echo json_encode($response);
+    } else {
+        echo json_encode(['error' => 'Data format error']);
+    }
 } else {
-    // Return an empty JSON response in case of an issue
-    echo json_encode(['error' => 'Unable to retrieve lists']);
+    echo json_encode(['error' => 'JSON file not found']);
 }
 ?>
