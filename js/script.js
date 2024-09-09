1efc7b767677632edd0c3c993499d673bf7571d0
baelinc <11509732+baelinc@users.noreply.github.com>
2024-09-09 10:06:55 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,15 +3,22 @@ $(document).ready(function() {
 
     function refreshLists() {
         $.ajax({
-            url: '/plugin.php?plugin=naughty-nice-list&file=api/get-lists.php&nopage=1',
-            method: 'GET',
-            success: function(data) {
-                let parsedData = JSON.parse(data);
-                updateGrid('naughty_list', parsedData.naughty);
-                updateGrid('nice_list', parsedData.nice);
-            }
-        });
+    url: '/plugin.php?plugin=naughty-nice-list&file=api/get-lists.php&nopage=1', // Update with your PHP script URL
+    method: 'GET',
+    dataType: 'json', // Expect JSON response
+    success: function(data) {
+        if (data.error) {
+            console.error('Error:', data.error);
+        } else {
+            // Handle valid data
+            console.log('Naughty List:', data.naughty);
+            console.log('Nice List:', data.nice);
+        }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+        console.error('AJAX Error:', textStatus, errorThrown);
     }
+});
 
     function updateGrid(elementId, listData) {
         const gridInstance = gridjs.Grid({
