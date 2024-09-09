7b28687f32525f7cd3bb547d6252fcfd470339de
baelinc <11509732+baelinc@users.noreply.github.com>
2024-09-09 09:23:47 -0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,16 @@
+<div style="margin:0 auto;"> <br />
+    <fieldset style="padding: 10px; border: 2px solid #000;">
+        <legend>Naughty Nice List Plugin Info</legend>
+        <div style="overflow: hidden; padding: 10px;">
+            <div id='credits'>
+                <b>Template Plugin Developed By:</b><br />
+                <br />
+                Johnathan Evans (baelinc)<br />
+                <br />
+                <a href='https://github.com/baelinc/Naughty-Nice-List/'>Git Repository</a><br>
+                <a href='https://github.com/baelinc/Naughty-Nice-List/issues'>Bug Reporter</a><br>
+                <br />
+            </div>
+        </div>
+    </fieldset>
+</div>
